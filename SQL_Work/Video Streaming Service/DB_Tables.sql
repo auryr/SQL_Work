9a24ee0c5f745eb658c437b431355e2dff78a40b
Aury Rodriguez <arm010286@hotmail.com>
2017-10-12 00:00:19 -0400

    Add table payments

--- SQL_Work/Video Streaming Service/DB_Tables.sql
+++ SQL_Work/Video Streaming Service/DB_Tables.sql
@@ -1,5 +1,11 @@
 Use Vidsi;
 
+
+CREATE TABLE VS_CATEGORIES(
+	id int NOT NULL PRIMARY KEY,
+	description varchar(100),
+);
+
 CREATE TABLE VS_SUBSCRIBERS(
 	id  int NOT NULL PRIMARY KEY,
 	firstname VARCHAR(100),
@@ -9,9 +15,11 @@ CREATE TABLE VS_SUBSCRIBERS(
 	address VARCHAR(255),
 	city   VARCHAR(50),
 	state   VARCHAR(50),
-	subscription_status VARCHAR(1),
 	initial_date date ,
-	free_streaming_end date,
+	free_plan_end date,
+	status VARCHAR(1),
+	category_id int,
+	FOREIGN KEY(category_id) REFERENCES VS_CATEGORIES(id)
 );
 
 go;
@@ -23,6 +31,8 @@ CREATE TABLE VS_SUBSCRIBERS_PHONE (
 	phone_type varchar(100),
 	phone_number varchar(20),
 	phone_extention varchar(5),
+	note text,
+	status VARCHAR(1),
 	FOREIGN KEY(subscriber_id) REFERENCES VS_SUBSCRIBERS(id)
 );
 
@@ -32,6 +42,7 @@ CREATE TABLE VS_SUBSCRIBERS_EMAILS (
 	id int NOT NULL PRIMARY KEY,
 	subscriber_id int,
 	email varchar(100),
+	status VARCHAR(1),
 	FOREIGN KEY(subscriber_id) REFERENCES VS_SUBSCRIBERS(id)
 );
 
@@ -52,6 +63,8 @@ CREATE TABLE VS_PLAN_TIERS (
 	description varchar(100),
 	cost decimal(10,2),
 	streaming_limit decimal(10,2),
+	extra_streaming_charge decimal(10,2),
+	status varchar(1),
 );
 
 CREATE TABLE VS_PLAN_DETAILS (
@@ -63,7 +76,7 @@ CREATE TABLE VS_PLAN_DETAILS (
 
 go;
 
-CREATE TABLE VS_SUBSRIBER_PLANS (
+CREATE TABLE VS_SUBSCRIBER_PLANS (
 	id int NOT NULL PRIMARY KEY,
 	subscriber_id int,
 	plan_tier_id int,
@@ -97,16 +110,10 @@ CREATE TABLE VS_GENRES(
 	description varchar(100),
 );
 
-CREATE TABLE VS_CATEGORIES(
-	id int NOT NULL PRIMARY KEY,
-	description varchar(100),
-);
-
 CREATE TABLE VS_AUTHORS(
 	id int NOT NULL PRIMARY KEY,
 	name varchar(100)
 );
-
 
 go;
 
@@ -116,18 +123,16 @@ CREATE TABLE VS_VIDEOS(
 	description text,
 	license_number varchar(100),
 	duration decimal(5,2),
-	stream_limit int,
-	category_id int,
+	view_limit int,
+	genre_id int,
 	content_provider_id int,
 	author_id int,
-	FOREIGN KEY(category_id)			REFERENCES VS_GENRES(id),
+	FOREIGN KEY(genre_id)			REFERENCES VS_GENRES(id),
 	FOREIGN KEY(content_provider_id)	REFERENCES VS_CONTENT_PROVIDERS(id),
 	FOREIGN KEY(author_id)				REFERENCES VS_AUTHORS(id),
 );
 
-
 go;
-
 
 CREATE TABLE SUBSCRIBERS_VIDEOS(
 	id int NOT NULL PRIMARY KEY,
@@ -139,8 +144,52 @@ CREATE TABLE SUBSCRIBERS_VIDEOS(
 );
 
 go;
+ 
+CREATE TABLE INVOICES(
+	id int NOT NULL PRIMARY KEY,
+	ref_number varchar(12),
+	date_issued date,
+	description varchar(100),
+	amount decimal (10,2),
+	expiration_date date,
+	status varchar(1),
+	subscriber_plan_id int,
+	FOREIGN KEY(subscriber_plan_id) REFERENCES VS_SUBSCRIBER_PLANS(id) 
+);
 
+go;
 
+CREATE TABLE PAYMENTS(
+	id int NOT NULL PRIMARY KEY,
+	ref_number varchar(12),
+	date_issued date,
+	description varchar(100),
+	amount decimal (10,2),
+	status varchar(1),
+	subscriber_id int,
+	FOREIGN KEY(subscriber_id) REFERENCES VS_SUBSCRIBERS(id),
+);
+
+go;
+
+CREATE TABLE PAYMENT_INVOICE(
+	id int NOT NULL PRIMARY KEY,
+	payment_id int,
+	invoice_id int,
+	amount decimal(10,2),
+	FOREIGN KEY(payment_id) REFERENCES PAYMENTS(id) ,
+	FOREIGN KEY(invoice_id) REFERENCES INVOICES(id) ,
+);
+
+CREATE TABLE PAYMENT_DETAILS(
+	id int NOT NULL PRIMARY KEY,
+	amount decimal(10,2),
+	document_no varchar(15) null,
+	payment_id int null,
+	subscriber_pay_card_id int null ,
+	FOREIGN KEY(payment_id) REFERENCES PAYMENTS(id) ,
+	FOREIGN KEY(subscriber_pay_card_id) REFERENCES VS_SUBSCRIBERS_PAY_CARDS(id)
+);
 
 
 
