66b6a536c4f15b30667bc6f84f36b800b2ae8417
Aury Rodriguez <arm010286@hotmail.com>
2017-10-12 02:38:34 -0400

    add str query

--- SQL_Work/Video Streaming Service/Query.sql
+++ SQL_Work/Video Streaming Service/Query.sql
@@ -1,8 +1,20 @@
 use Vidsi; 
-SELECT subscriber_id, COUNT(video_id) video_views ,
-ROW_NUMBER()OVER(PARTITION BY subscriber_id ORDER BY subscriber_id,video_id)
-FROM tvsSubscribersStreaming
-GROUP BY subscriber_id,video_id,CONCAT(DATEPART(month, streaming_date) ,'/', DATEPART(year, streaming_date))
+DECLARE @stream_date_i date = '2017-01-01';
+DECLARE @stream_date_f date = '2017-01-31';
+
+SELECT  ss.video_rank, vi.title, ss.video_streams, su.id,  su.firstname , su.lastname from
+(
+	SELECT subscriber_id, video_id, COUNT(video_id) video_streams ,
+	ROW_NUMBER()OVER(PARTITION BY subscriber_id ORDER BY subscriber_id,COUNT(video_id) desc) as video_rank
+	FROM tvsSubscribersStreaming
+
+	GROUP BY subscriber_id,video_id,CONCAT(DATEPART(month, streaming_date) ,'/', DATEPART(year, streaming_date))
+) as ss 
+INNER JOIN tvssubscribers su 
+	ON ss.subscriber_id = su.id
+INNER JOIN tvsvideos vi
+	ON ss.video_id = vi.id
+ORDER BY 
 
 
 
